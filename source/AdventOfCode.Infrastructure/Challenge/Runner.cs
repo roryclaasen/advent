d96524a307b767bcc78e94a6714883253fce8e6e
Rory Claasen <rory@claasen.biz>
2022-06-27 22:45:04 +0100

    Adds initial project

---
+++ source/AdventOfCode.Infrastructure/Challenge/Runner.cs
@@ -0,0 +1,19 @@
+﻿namespace AdventOfCode.Infrastructure
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public abstract class Runner<TChallenge> : IRunner
+        where TChallenge : Challenge, new()
+    {
+        protected Lazy<TChallenge> Challenge = new Lazy<TChallenge>(() => new());
+
+        public abstract int Year { get; }
+
+        public abstract int Day { get; }
+
+        public abstract Task<string> SolvePart1();
+
+        public abstract Task<string> SolvePart2();
+    }
+}
---
+++ source/AdventOfCode/Program.cs
@@ -0,0 +1,69 @@
+﻿using AdventOfCode.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+IEnumerable<Assembly> GetAdventAssemblies()
+{
+    var directoryInfo = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
+    var moduleFiles = directoryInfo.GetFiles("AdventOfCode.*.dll");
+
+    foreach (var file in moduleFiles)
+    {
+        yield return Assembly.Load(Path.GetFileNameWithoutExtension(file.Name));
+    }
+}
+
+IEnumerable<Type> FindAllOfType<T>(IEnumerable<Assembly> assemblies)
+{
+    var baseType = typeof(T);
+
+    return assemblies.SelectMany(a => a.GetTypes())
+         .Where(t => t.IsClass && !t.IsAbstract)
+         .Where(p => baseType.IsAssignableFrom(p));
+}
+
+Dictionary<int, List<IRunner>> GetRunners()
+{
+    var dictionary = new Dictionary<int, List<IRunner>>();
+    var runners = FindAllOfType<IRunner>(GetAdventAssemblies());
+    foreach (var runnerType in runners)
+    {
+        var runnerObj = Activator.CreateInstance(runnerType);
+        if (runnerObj is IRunner runner)
+        {
+            if (!dictionary.ContainsKey(runner.Year))
+            {
+                dictionary.Add(runner.Year, new List<IRunner>());
+            }
+
+            dictionary[runner.Year].Add(runner);
+        }
+        else
+        {
+            // TODO Throw
+        }
+    }
+
+
+    return dictionary;
+}
+
+var allRunners = GetRunners();
+foreach (var year in allRunners.Keys)
+{
+    Console.WriteLine($"Answers for {year}");
+    var runners = allRunners[year].OrderBy(r => r.Day);
+    foreach(var runner in runners)
+    {
+        Console.WriteLine($"  Answers for day {runner.Day}");
+        
+        var part1 = await runner.SolvePart1();
+        Console.WriteLine($"    Part 1:\n{part1}\n");
+
+        var part2 = await runner.SolvePart2();
+        Console.WriteLine($"    Part 2:\n{part2}\n");
+    }
+}
