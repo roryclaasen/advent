53eb516f58233e99cbb124e20fae45effc30d5aa
Rory Claasen <rory@claasen.biz>
2022-06-28 20:05:52 +0100

    Coverts to single input runner

--- source/AdventOfCode.Infrastructure/Runner/Runner.cs
+++ source/AdventOfCode.Infrastructure/Runner/Runner.cs
@@ -6,14 +6,16 @@
     public abstract class Runner<TChallenge> : IRunner
         where TChallenge : IChallenge, new()
     {
-        protected Lazy<TChallenge> Challenge = new Lazy<TChallenge>(() => new());
+        private readonly Lazy<TChallenge> challenge = new Lazy<TChallenge>(() => new());
 
         public abstract int Year { get; }
 
         public abstract int Day { get; }
 
-        public abstract Task<string> SolvePart1();
+        public abstract string Input { get; }
 
-        public abstract Task<string> SolvePart2();
+        public Task<string> SolvePart1() => this.challenge.Value.SolvePart1(this.Input);
+
+        public Task<string> SolvePart2() => this.challenge.Value.SolvePart2(this.Input);
     }
 }
