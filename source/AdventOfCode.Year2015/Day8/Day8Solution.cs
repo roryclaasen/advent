c6ef9e1dc132b99c109145a53d47f574acf745b9
Rory Claasen <rory@claasen.biz>
2023-11-08 15:41:18 +0000

    Adds 2015 Day 8

---
+++ source/AdventOfCode.Year2015/Day8/Day8Solution.cs
@@ -0,0 +1,99 @@
+namespace AdventOfCode.Year2015;
+
+using AdventOfCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+[Problem(2015, 8, "Matchsticks")]
+public class Day8Solution : ISolver
+{
+    public object? PartOne(string input)
+    {
+        var entries = input.Lines().Select(l => new Entry(l, Decode(l)));
+        var rawLength = entries.Sum(e => e.Raw.Length);
+        var parsedLength = entries.Sum(e => e.Parsed.Length);
+
+        return rawLength - parsedLength;
+    }
+
+    public object? PartTwo(string input)
+    {
+        var entries = input.Lines().Select(l => new Entry(l, Encode(l)));
+        var rawLength = entries.Sum(e => e.Raw.Length);
+        var parsedLength = entries.Sum(e => e.Parsed.Length);
+
+        return parsedLength - rawLength;
+    }
+
+    static string Decode(string input)
+    {
+        if (!input.StartsWith('"') || !input.EndsWith('"'))
+        {
+            throw new Exception("Invalid input string");
+        }
+
+        var trimed = input[1..^1];
+        var sb = new StringBuilder();
+        for (var i = 0; i < trimed.Length;)
+        {
+            var currentHead = trimed[i];
+            if (currentHead == '\\')
+            {
+                if (i == trimed.Length - 1)
+                {
+                    throw new Exception("Invalid escape sequence");
+                }
+
+                var next = trimed[i + 1];
+                if (next == '\\' || next == '"')
+                {
+                    sb.Append(next);
+                    i += 2;
+                }
+                else if (next == 'x')
+                {
+                    var hex = trimed[(i + 2)..(i + 4)];
+                    var decValue = uint.Parse(hex, NumberStyles.HexNumber);
+                    var character = Convert.ToChar(decValue);
+                    sb.Append(character);
+                    i += 4;
+                }
+                else
+                {
+                    throw new Exception("Invalid escape sequence");
+                }
+            }
+            else
+            {
+                sb.Append(currentHead);
+                i++;
+            }
+        }
+        return sb.ToString();
+    }
+
+    static string Encode(string input)
+    {
+        var sb = new StringBuilder();
+        sb.Append('"');
+        foreach (var character in input)
+        {
+            var encodedChar = character switch
+            {
+                '"' => @"\""",
+                '\\' => @"\\",
+                _ => character.ToString()
+            };
+
+            sb.Append(encodedChar);
+        }
+        sb.Append('"');
+        return sb.ToString();
+    }
+
+    record Entry(string Raw, string Parsed);
+}
---
+++ tests/AdventOfCode.Year2015.Tests/Day8SolutionTests.cs
@@ -0,0 +1,33 @@
+namespace AdventOfCode.Year2015.Tests;
+
+using AdventOfCode.Shared.Tests;
+
+[TestClass]
+public class Day8SolutionTests : SolverBaseTests<Day8Solution>
+{
+    [TestMethod]
+    [DataRow("\"\"", "2")]
+    [DataRow("\"abc\"", "2")]
+    [DataRow("\"aaa\\\"aaa\"", "3")]
+    [DataRow("\"\\x27\"", "5")]
+    public void SolvePart1(string input, string answer)
+    {
+        Assert.IsNotNull(this.Solver);
+
+        var solvedAnswer = this.Solver.PartOne(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+
+    [TestMethod]
+    [DataRow("\"\"", "4")]
+    [DataRow("\"abc\"", "4")]
+    [DataRow("\"aaa\\\"aaa\"", "6")]
+    [DataRow("\"\\x27\"", "5")]
+    public void SolvePart2(string input, string answer)
+    {
+        Assert.IsNotNull(this.Solver);
+
+        var solvedAnswer = this.Solver.PartTwo(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+}
