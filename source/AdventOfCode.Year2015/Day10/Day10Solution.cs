819acb395e56ec4d020517a5eba0c58a63fac6ca
Rory Claasen <rory@claasen.biz>
2023-11-11 16:37:50 +0000

    Adds 2015 Day 10

---
+++ source/AdventOfCode.Year2015/Day10/Day10Solution.cs
@@ -0,0 +1,49 @@
+namespace AdventOfCode.Year2015;
+
+using AdventOfCode.Shared;
+using System.Text;
+
+[Problem(2015, 10, "Elves Look, Elves Say")]
+public class Day10Solution : ISolver
+{
+    public object? PartOne(string input)
+    {
+        for (var i = 0; i < 40; i++)
+        {
+            input = LookAndSay(input);
+        }
+
+        return input.Length;
+    }
+
+    public object? PartTwo(string input)
+    {
+        for (var i = 0; i < 50; i++)
+        {
+            input = LookAndSay(input);
+        }
+
+        return input.Length;
+    }
+
+    public string LookAndSay(string input)
+    {
+        var finalString = new StringBuilder();
+
+        for (var i = 0; i < input.Length; i++)
+        {
+            var currentNumber = input[i];
+            var count = 1;
+            while (i + 1 < input.Length && input[i + 1] == currentNumber)
+            {
+                count++;
+                i++;
+            }
+
+            finalString.Append(count);
+            finalString.Append(currentNumber);
+        }
+
+        return finalString.ToString();
+    }
+}
---
+++ tests/AdventOfCode.Year2015.Tests/Day10SolutionTests.cs
@@ -0,0 +1,21 @@
+namespace AdventOfCode.Year2015.Tests;
+
+using AdventOfCode.Shared.Tests;
+
+[TestClass]
+public class Day10SolutionTests : SolverBaseTests<Day10Solution>
+{
+    [TestMethod]
+    [DataRow("1", "11")]
+    [DataRow("11", "21")]
+    [DataRow("21", "1211")]
+    [DataRow("1211", "111221")]
+    [DataRow("111221", "312211")]
+    public void LookAndSay(string input, string answer)
+    {
+        Assert.IsNotNull(this.Solver);
+
+        var solvedAnswer = this.Solver.LookAndSay(input);
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+}
