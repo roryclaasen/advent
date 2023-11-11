57adbbb33776296f030be5473e879fca86df5b55
Rory Claasen <rory@claasen.biz>
2023-11-11 19:38:23 +0000

    Refactors Permutations.cs

--- source/AdventOfCode.Shared/Permutations.cs
+++ source/AdventOfCode.Shared/Permutations.cs
@@ -5,24 +5,23 @@ namespace AdventOfCode.Shared
 
     public static class Permutations
     {
-        public static List<List<T>> GetPermutations<T>(IEnumerable<T> values)
+        public static List<List<T>> GetPermutations<T>(IEnumerable<T> values, T? start = null, T? end = null) where T : class
         {
             var permutations = new List<List<T>>();
-            Permute(values, new HashSet<T>(), new List<T>(), permutations);
+            var visited = new HashSet<T>();
+            var permutation = new List<T>();
+            if (start is not null)
+            {
+                visited.Add(start);
+                permutation.Add(start);
+            }
+            Permute(values, visited, permutation, permutations, end);
             return permutations;
         }
 
-        public static List<List<T>> GetPermutations<T>(IEnumerable<T> values, T start, T end)
+        private static void Permute<T>(IEnumerable<T> values, HashSet<T> visited, List<T> permutation, List<List<T>> permutations, T? end = null) where T : class
         {
-            var permutations = new List<List<T>>();
-            var visited = new HashSet<T> { start };
-            Permute(values, visited, new List<T> { start }, permutations, end);
-            return permutations;
-        }
-
-        private static void Permute<T>(IEnumerable<T> values, HashSet<T> visited, List<T> permutation, List<List<T>> permutations, T end)
-        {
-            if (permutation.Count == values.Count() && permutation.Last()!.Equals(end))
+            if (permutation.Count == values.Count() && (end is null || permutation.Last()!.Equals(end)))
             {
                 permutations.Add(permutation);
                 return;
@@ -35,26 +34,6 @@ namespace AdventOfCode.Shared
                     visited.Add(value);
                     var newPermutation = new List<T>(permutation) { value };
                     Permute(values, visited, newPermutation, permutations, end);
-                    visited.Remove(value);
-                }
-            }
-        }
-
-        private static void Permute<T>(IEnumerable<T> values, HashSet<T> visited, List<T> permutation, List<List<T>> permutations)
-        {
-            if (permutation.Count == values.Count())
-            {
-                permutations.Add(permutation);
-                return;
-            }
-
-            foreach (var value in values)
-            {
-                if (!visited.Contains(value))
-                {
-                    visited.Add(value);
-                    var newPermutation = new List<T>(permutation) { value };
-                    Permute(values, visited, newPermutation, permutations);
                     visited.Remove(value);
                 }
             }
