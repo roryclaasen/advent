22d26552a3f25c4fd2b648c7bb0461d851c53629
Rory Claasen <rory@claasen.biz>
2023-11-11 18:01:31 +0000

    adds 2015 day 12

---
+++ source/AdventOfCode.Year2015/Day12/Day12Solution.cs
@@ -0,0 +1,43 @@
+namespace AdventOfCode.Year2015;
+
+using AdventOfCode.Shared;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.RegularExpressions;
+
+[Problem(2015, 12, null)]
+public partial class Day12Solution : ISolver
+{
+    public object? PartOne(string input)
+    {
+        var numbers = NumberRegex()
+            .Matches(input)
+            .Select(m => int.Parse(m.Value));
+        return numbers.Sum();
+    }
+
+    public object? PartTwo(string input)
+    {
+        var jsonObject = JsonNode.Parse(input) ?? throw new JsonException("Failed to parse input");
+        return CountNumbersWithoutRed(jsonObject);
+    }
+
+    private int CountNumbersWithoutRed(JsonNode? node) => node switch
+    {
+        JsonObject jsonObject when !this.HasRed(jsonObject) => jsonObject.Select(j => j.Value).Sum(this.CountNumbersWithoutRed),
+        JsonValue jsonValue when jsonValue.TryGetValue<int>(out var value) => value,
+        JsonArray jsonArray => jsonArray.Sum(this.CountNumbersWithoutRed),
+        _ => 0
+    };
+
+    private bool HasRed(JsonNode? node) => node switch
+    {
+        JsonValue jsonValue => jsonValue.TryGetValue<string>(out var value) && value == "red",
+        JsonObject jsonObject => jsonObject.Select(j => j.Value).OfType<JsonValue>().Any(this.HasRed),
+        _ => false
+    };
+
+    [GeneratedRegex("-?\\d+")]
+    private static partial Regex NumberRegex();
+}
---
+++ tests/AdventOfCode.Year2015.Tests/Day12SolutionTests.cs
@@ -0,0 +1,37 @@
+namespace AdventOfCode.Year2015.Tests;
+
+using AdventOfCode.Shared.Tests;
+
+[TestClass]
+public class Day12SolutionTests : SolverBaseTests<Day12Solution>
+{
+    [TestMethod]
+    [DataRow("[1,2,3]", "6")]
+    [DataRow("{\"a\":2,\"b\":4}", "6")]
+    [DataRow("[[[3]]]", "3")]
+    [DataRow("{\"a\":{\"b\":4},\"c\":-1}", "3")]
+    [DataRow("{\"a\":[-1,1]}", "0")]
+    [DataRow("[-1,{\"a\":1}]", "0")]
+    [DataRow("[]", "0")]
+    [DataRow("{}", "0")]
+    public void SolvePart1(string input, string answer)
+    {
+        Assert.IsNotNull(this.Solver);
+
+        var solvedAnswer = this.Solver.PartOne(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+
+    [TestMethod]
+    [DataRow("[1,2,3]", "6")]
+    [DataRow("[1,{\"c\":\"red\",\"b\":2},3]", "4")]
+    [DataRow("{\"d\":\"red\",\"e\":[1,2,3,4],\"f\":5}", "0")]
+    [DataRow("[1,\"red\",5]", "6")]
+    public void SolvePart2(string input, string answer)
+    {
+        Assert.IsNotNull(this.Solver);
+
+        var solvedAnswer = this.Solver.PartTwo(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+}
