ac7d3ce096ba6c226b4c593a57a33582b22a7861
Rory Claasen <rory@xbox.com>
2023-11-14 16:38:10 +0000

    Adds year 2015 day 15

---
+++ tests/AdventOfCode.Year2015.Tests/Day15SolutionTests.cs
@@ -0,0 +1,33 @@
+namespace AdventOfCode.Year2015.Tests;
+
+using AdventOfCode.Shared.Tests;
+
+[TestClass]
+public class Day15SolutionTests : SolverBaseTests<Day15Solution>
+{
+    [TestMethod]
+    [DataRow(@"Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8
+Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3
+Fake: capacity 0, durability 0, flavor 0, texture 0, calories 0
+Fake: capacity 0, durability 0, flavor 0, texture 0, calories 0", "62842880")]
+    public void SolvePart1(string input, string answer)
+    {
+        Assert.IsNotNull(this.Solver);
+
+        var solvedAnswer = this.Solver.PartOne(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+
+    [TestMethod]
+    [DataRow(@"Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8
+Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3
+Fake: capacity 0, durability 0, flavor 0, texture 0, calories 0
+Fake: capacity 0, durability 0, flavor 0, texture 0, calories 0", "57600000")]
+    public void SolvePart2(string input, string answer)
+    {
+        Assert.IsNotNull(this.Solver);
+
+        var solvedAnswer = this.Solver.PartTwo(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+}
