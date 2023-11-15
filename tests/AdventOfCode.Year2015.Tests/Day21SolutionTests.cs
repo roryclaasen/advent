193a6aa48576889bcb232b818bfd1e3145fd5b9c
Rory Claasen <rory@claasen.biz>
2023-11-15 16:46:14 +0000

    Adds 2015 Day 21

---
+++ tests/AdventOfCode.Year2015.Tests/Day21SolutionTests.cs
@@ -0,0 +1,27 @@
+namespace AdventOfCode.Year2015.Tests;
+
+using AdventOfCode.Shared.Tests;
+
+[TestClass]
+public class Day21SolutionTests : SolverBaseTests<Day21Solution>
+{
+    [TestMethod]
+    [DataRow("", "")]
+    public void SolvePart1(string input, string answer)
+    {
+        Assert.IsNotNull(this.Solver);
+
+        var solvedAnswer = this.Solver.PartOne(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+
+    [TestMethod]
+    [DataRow("", "")]
+    public void SolvePart2(string input, string answer)
+    {
+        Assert.IsNotNull(this.Solver);
+
+        var solvedAnswer = this.Solver.PartTwo(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+}
