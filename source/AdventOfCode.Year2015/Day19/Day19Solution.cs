814e0f7479d13d27fef0fa8176a0e7f685448572
Rory Claasen <rory@claasen.biz>
2023-11-15 12:07:08 +0000

    Adds 2015 Day 19

---
+++ source/AdventOfCode.Year2015/Day19/Day19Solution.cs
@@ -0,0 +1,84 @@
+namespace AdventOfCode.Year2015;
+
+using AdventOfCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+[Problem(2015, 19, "Medicine for Rudolph")]
+public class Day19Solution : ISolver
+{
+    public object? PartOne(string input)
+    {
+        var (molecule, replacements) = this.ParseInput(input);
+
+        var molecules = this.GetAllNewMolecules(molecule, replacements);
+        return molecules.Count;
+    }
+
+    public object? PartTwo(string input)
+    {
+        var (moleculeGoal, replacements) = this.ParseInput(input);
+
+        var currentMolecules = new HashSet<string> { moleculeGoal };
+        var step = 0;
+        while (!currentMolecules.Contains("e"))
+        {
+            step++;
+            currentMolecules = currentMolecules
+                .SelectMany(m => this.GetAllPreviousMolecules(m, replacements))
+                .OrderBy(step => step.Length)
+                .Take(1000)
+                .ToHashSet();
+        }
+
+        return step;
+    }
+
+    HashSet<string> GetAllNewMolecules(string molecule, IEnumerable<Replacement> replacements)
+    {
+        var molecules = new HashSet<string>();
+        foreach (var replacement in replacements)
+        {
+            foreach (var character in molecule.AllIndexesOf(replacement.From))
+            {
+                var newMolecule = molecule[..character] + replacement.To + molecule[(character + replacement.From.Length)..];
+                molecules.Add(newMolecule);
+            }
+        }
+
+        return molecules;
+    }
+
+    HashSet<string> GetAllPreviousMolecules(string molecule, IEnumerable<Replacement> replacements)
+    {
+        var molecules = new HashSet<string>();
+        foreach (var replacement in replacements)
+        {
+            foreach (var character in molecule.AllIndexesOf(replacement.To))
+            {
+                var newMolecule = molecule[..character] + replacement.From + molecule[(character + replacement.To.Length)..];
+                molecules.Add(newMolecule);
+            }
+        }
+
+        return molecules;
+    }
+
+    (string Molecule, IEnumerable<Replacement> Replacements) ParseInput(string input)
+    {
+        var parts = input.Split(Environment.NewLine + Environment.NewLine);
+        return (parts[1], ParseReplacements(parts[0]));
+    }
+
+    IEnumerable<Replacement> ParseReplacements(string input)
+    {
+        foreach (var line in input.Lines())
+        {
+            var parts = line.Split(" => ");
+            yield return new Replacement(parts[0], parts[1]);
+        }
+    }
+
+    record Replacement(string From, string To);
+}
---
+++ tests/AdventOfCode.Year2015.Tests/Day19SolutionTests.cs
@@ -0,0 +1,42 @@
+namespace AdventOfCode.Year2015.Tests;
+
+using AdventOfCode.Shared.Tests;
+
+[TestClass]
+public class Day19SolutionTests : SolverBaseTests<Day19Solution>
+{
+    [TestMethod]
+    [DataRow(@"H => HO
+H => OH
+O => HH
+
+HOH", "4")]
+[DataRow(@"H => HO
+H => OH
+O => HH
+
+HOHOHO", "7")]
+    public void SolvePart1(string input, string answer)
+    {
+        Assert.IsNotNull(this.Solver);
+
+        var solvedAnswer = this.Solver.PartOne(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+
+    [TestMethod]
+    [DataRow(@"e => H
+e => O
+H => HO
+H => OH
+O => HH
+
+HOHOHO", "6")]
+    public void SolvePart2(string input, string answer)
+    {
+        Assert.IsNotNull(this.Solver);
+
+        var solvedAnswer = this.Solver.PartTwo(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+}
