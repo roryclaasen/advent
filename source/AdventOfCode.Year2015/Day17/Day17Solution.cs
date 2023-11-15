49f011123f6db5cbfc7221404824f3afd6f641ed
Rory Claasen <rory@claasen.biz>
2023-11-15 10:33:02 +0000

    Adds 2015 Day 17

---
+++ source/AdventOfCode.Year2015/Day17/Day17Solution.cs
@@ -0,0 +1,58 @@
+namespace AdventOfCode.Year2015;
+
+using AdventOfCode.Shared;
+using System.Collections.Generic;
+using System.Linq;
+
+[Problem(2015, 17, "No Such Thing as Too Much")]
+public class Day17Solution : ISolver
+{
+    public int MaxLiters { get; set; } = 150;
+
+    public object? PartOne(string input)
+    {
+        var index = 0;
+        var containers = input.Lines().Select(int.Parse).ToDictionary(_ => index++, i => i);
+
+        var combinations = this.FindAllCombinations(containers, this.MaxLiters);
+
+        return combinations.Count;
+    }
+
+    public object? PartTwo(string input)
+    {
+        var index = 0;
+        var containers = input.Lines().Select(int.Parse).ToDictionary(_ => index++, i => i);
+
+        var combinations = this.FindAllCombinations(containers, this.MaxLiters);
+
+        return combinations.MinBy(c => c.Length)?.Length;
+    }
+
+    List<int[]> FindAllCombinations(Dictionary<int, int> numbers, int target)
+    {
+        var combinations = new List<int[]>();
+
+        foreach (var number in numbers)
+        {
+            var remaining = target - number.Value;
+
+            if (remaining == 0)
+            {
+                combinations.Add([number.Value]);
+            }
+            else if (remaining > 0)
+            {
+                var remainingNumbers = numbers.Where(n => n.Key > number.Key).ToDictionary(n => n.Key, n => n.Value);
+                var remainingCombinations = this.FindAllCombinations(remainingNumbers, remaining);
+
+                foreach (var combination in remainingCombinations)
+                {
+                    combinations.Add([number.Value, .. combination]);
+                }
+            }
+        }
+
+        return combinations;
+    }
+}
