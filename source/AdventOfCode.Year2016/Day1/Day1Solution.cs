88391aa5e49bf7f67c9a709946ef5417e15ced71
Rory Claasen <rory@xbox.com>
2023-11-24 16:27:40 +0000

    Adds 2016 day 1

---
+++ source/AdventOfCode.Year2016/Day1/Day1Solution.cs
@@ -0,0 +1,86 @@
+namespace AdventOfCode.Year2016;
+
+using AdventOfCode.Shared;
+using System;
+using System.Collections.Generic;
+
+[Problem(2016, 1, "No Time for a Taxicab")]
+public class Day1Solution : ISolver
+{
+    public object? PartOne(string input)
+    {
+        var instructions = ParseInput(input);
+        var position = (x: 0, y: 0);
+        var direction = 0;
+
+        foreach (var instruction in instructions)
+        {
+            direction = instruction.Direction switch
+            {
+                'L' => (direction + 3) % 4,
+                'R' => (direction + 1) % 4,
+                _ => throw new Exception("Invalid direction")
+            };
+
+            position = direction switch
+            {
+                0 => (position.x, position.y + instruction.Distance),
+                1 => (position.x + instruction.Distance, position.y),
+                2 => (position.x, position.y - instruction.Distance),
+                3 => (position.x - instruction.Distance, position.y),
+                _ => throw new Exception("Invalid direction")
+            };
+        }
+
+        return Math.Abs(position.x) + Math.Abs(position.y);
+    }
+
+    public object? PartTwo(string input)
+    {
+        var instructions = ParseInput(input);
+        var position = (x: 0, y: 0);
+        var direction = 0;
+
+        var visited = new HashSet<(int x, int y)> { position };
+
+        foreach (var instruction in instructions)
+        {
+            direction = instruction.Direction switch
+            {
+                'L' => (direction + 3) % 4,
+                'R' => (direction + 1) % 4,
+                _ => throw new Exception("Invalid direction")
+            };
+
+            for (var i = 0; i < instruction.Distance; i++)
+            {
+                position = direction switch
+                {
+                    0 => (position.x, position.y + 1),
+                    1 => (position.x + 1, position.y),
+                    2 => (position.x, position.y - 1),
+                    3 => (position.x - 1, position.y),
+                    _ => throw new Exception("Invalid direction")
+                };
+
+                if (visited.Contains(position))
+                {
+                    return Math.Abs(position.x) + Math.Abs(position.y);
+                }
+                visited.Add(position);
+            }
+        }
+
+        throw new Exception("No location visited twice");
+    }
+
+    IEnumerable<Instruction> ParseInput(string input)
+    {
+        foreach (var instruction in input.Split(", "))
+        {
+            yield return new Instruction(instruction[0], int.Parse(instruction[1..]));
+        }
+    }
+
+    record Instruction(char Direction, int Distance);
+}
---
+++ tests/AdventOfCode.Year2016.Tests/Day1SolutionTests.cs
@@ -0,0 +1,29 @@
+namespace AdventOfCode.Year2016.Tests;
+
+using AdventOfCode.Shared.Tests;
+
+[TestClass]
+public class Day1SolutionTests : SolverBaseTests<Day1Solution>
+{
+    [TestMethod]
+    [DataRow("R2, L3", "5")]
+    [DataRow("R2, R2, R2", "2")]
+    [DataRow("R5, L5, R5, R3", "12")]
+    public void SolvePart1(string input, string answer)
+    {
+        Assert.IsNotNull(this.Solver);
+
+        var solvedAnswer = this.Solver.PartOne(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+
+    [TestMethod]
+    [DataRow("R8, R4, R4, R8", "4")]
+    public void SolvePart2(string input, string answer)
+    {
+        Assert.IsNotNull(this.Solver);
+
+        var solvedAnswer = this.Solver.PartTwo(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+}
