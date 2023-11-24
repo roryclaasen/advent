ed8b8b83fc10f10e2e2903d757b79e858f3145c1
Rory Claasen <rory@xbox.com>
2023-11-24 17:13:55 +0000

    Adds 2016 day 2

---
+++ source/AdventOfCode.Year2016/Day2/Day2Solution.cs
@@ -0,0 +1,104 @@
+namespace AdventOfCode.Year2016;
+
+using AdventOfCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+[Problem(2016, 2, "Bathroom Security")]
+public class Day2Solution : ISolver
+{
+    public object? PartOne(string input)
+    {
+        var keypad = new char[3, 3]
+        {
+            { '1', '2', '3' },
+            { '4', '5', '6' },
+            { '7', '8', '9' }
+        };
+
+        var sb = new StringBuilder();
+
+        var x = 1;
+        var y = 1;
+        foreach (var line in ParseInput(input))
+        {
+            foreach (var direction in line)
+            {
+                switch (direction)
+                {
+                    case Direction.Up:
+                        y = Math.Max(0, y - 1);
+                        break;
+                    case Direction.Right:
+                        x = Math.Min(2, x + 1);
+                        break;
+                    case Direction.Down:
+                        y = Math.Min(2, y + 1);
+                        break;
+                    case Direction.Left:
+                        x = Math.Max(0, x - 1);
+                        break;
+                }
+            }
+
+            sb.Append(keypad[y, x]);
+        }
+
+        return sb.ToString();
+    }
+
+    public object? PartTwo(string input)
+    {
+        var keypad = new char?[5, 5]
+        {
+            { null, null, '1', null, null},
+            { null, '2', '3', '4', null },
+            { '5', '6', '7', '8', '9' },
+            { null, 'A', 'B', 'C', null },
+            { null, null, 'D', null, null},
+        };
+
+        var sb = new StringBuilder();
+
+        var x = 0;
+        var y = 3;
+        foreach (var line in ParseInput(input))
+        {
+            foreach (var direction in line)
+            {
+                var nextX = x;
+                var nextY = y;
+                switch (direction)
+                {
+                    case Direction.Up:
+                        nextY = Math.Max(0, y - 1);
+                        break;
+                    case Direction.Right:
+                        nextX = Math.Min(4, x + 1);
+                        break;
+                    case Direction.Down:
+                        nextY = Math.Min(4, y + 1);
+                        break;
+                    case Direction.Left:
+                        nextX = Math.Max(0, x - 1);
+                        break;
+                }
+
+                if (keypad[nextY, nextX] != null)
+                {
+                    x = nextX;
+                    y = nextY;
+                }
+            }
+
+            sb.Append(keypad[y, x]);
+        }
+
+        return sb.ToString();
+    }
+
+    static IEnumerable<IEnumerable<Direction>> ParseInput(string input)
+        => input.Lines().Select(l => l.Select(c => (Direction)c));
+}
---
+++ tests/AdventOfCode.Year2016.Tests/Day2SolutionTests.cs
@@ -0,0 +1,33 @@
+namespace AdventOfCode.Year2016.Tests;
+
+using AdventOfCode.Shared.Tests;
+
+[TestClass]
+public class Day2SolutionTests : SolverBaseTests<Day2Solution>
+{
+    [TestMethod]
+    [DataRow(@"ULL
+RRDDD
+LURDL
+UUUUD", "1985")]
+    public void SolvePart1(string input, string answer)
+    {
+        Assert.IsNotNull(this.Solver);
+
+        var solvedAnswer = this.Solver.PartOne(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+
+    [TestMethod]
+    [DataRow(@"ULL
+RRDDD
+LURDL
+UUUUD", "5DB3")]
+    public void SolvePart2(string input, string answer)
+    {
+        Assert.IsNotNull(this.Solver);
+
+        var solvedAnswer = this.Solver.PartTwo(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+}
