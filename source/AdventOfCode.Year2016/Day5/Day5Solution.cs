5a54182543aedd20ef28a66521152efa66f1042f
Rory Claasen <rory@xbox.com>
2023-11-27 11:44:23 +0000

    Adds 2016 Day 5

---
+++ source/AdventOfCode.Year2016/Day5/Day5Solution.cs
@@ -0,0 +1,68 @@
+namespace AdventOfCode.Year2016;
+
+using AdventOfCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+[Problem(2016, 5, "How About a Nice Game of Chess?")]
+public class Day5Solution : ISolver
+{
+    private readonly Dictionary<string, string> hashCache = [];
+
+    public object? PartOne(string input)
+    {
+        var password = new StringBuilder();
+        var index = 0;
+        while (password.Length < 8)
+        {
+            var hashString = GetHash($"{input}{index}");
+            if (hashString.StartsWith("00000"))
+            {
+                password.Append(hashString[5]);
+            }
+
+            index++;
+        }
+
+        return password.ToString();
+    }
+
+    public object? PartTwo(string input)
+    {
+        var index = 0;
+        var found = 0;
+        var chars = new char[8];
+        while (found < 8)
+        {
+            var hashString = GetHash($"{input}{index}");
+            if (hashString.StartsWith("00000"))
+            {
+                var position = hashString[5] - '0';
+                if (position < 8 && chars[position] == '\0')
+                {
+                    chars[position] = hashString[6];
+                    found++;
+                }
+            }
+
+            index++;
+        }
+
+        return new string(chars);
+    }
+
+    private string GetHash(string input)
+    {
+        if (hashCache.TryGetValue(input, out var hashString))
+        {
+            return hashString;
+        }
+
+        var hash = MD5.HashData(Encoding.ASCII.GetBytes(input));
+        hashString = BitConverter.ToString(hash).Replace("-", string.Empty).ToLowerInvariant();
+        hashCache.Add(input, hashString);
+        return hashString;
+    }
+}
---
+++ tests/AdventOfCode.Year2016.Tests/Day5SolutionTests.cs
@@ -0,0 +1,29 @@
+namespace AdventOfCode.Year2016.Tests;
+
+using AdventOfCode.Shared.Tests;
+
+[TestClass]
+public class Day5SolutionTests : SolverBaseTests<Day5Solution>
+{
+    [TestMethod]
+    [Ignore("Test takes too long to run")]
+    [DataRow("abc", "18f47a30")]
+    public void SolvePart1(string input, string answer)
+    {
+        Assert.IsNotNull(this.Solver);
+
+        var solvedAnswer = this.Solver.PartOne(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+
+    [TestMethod]
+    [Ignore("Test takes too long to run")]
+    [DataRow("abc", "05ace8e3")]
+    public void SolvePart2(string input, string answer)
+    {
+        Assert.IsNotNull(this.Solver);
+
+        var solvedAnswer = this.Solver.PartTwo(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+}
