1ed1362b6d0ae1a25629055b117f577f4e52b786
Rory Claasen <rory@xbox.com>
2023-12-02 14:37:45 +0000

    Adds 2023 Day 2

---
+++ source/AdventOfCode.Year2023/Day2/Day2Solution.cs
@@ -0,0 +1,83 @@
+namespace AdventOfCode.Year2023;
+
+using AdventOfCode.Shared;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+[Problem(2023, 2, "Cube Conundrum")]
+public partial class Day2Solution : ISolver
+{
+    public object? PartOne(string input)
+    {
+        var games = ParseInput(input)
+            .Where(g => !g.Reveals.Any(r => r.Red > 12))
+            .Where(g => !g.Reveals.Any(r => r.Blue > 14))
+            .Where(g => !g.Reveals.Any(r => r.Green > 13));
+        return games.Sum(g => g.Id);
+    }
+
+    public object? PartTwo(string input)
+    {
+        static int MinCubes(Game game, Func<GameReveal, int> selector)
+            => Math.Max(1, game.Reveals.Select(selector).Max());
+
+        var games = ParseInput(input)
+            .Select(g => MinCubes(g, r => r.Red) * MinCubes(g, r => r.Blue) * MinCubes(g, r => r.Green));
+
+        return games.Sum();
+    }
+
+    IEnumerable<Game> ParseInput(string input)
+    {
+        foreach (var line in input.Lines())
+        {
+            var part = line.Split(':');
+            var id = int.Parse(part[0].Replace("Game ", string.Empty));
+            var bags = part[1].Split(';').Select(GameReveal.ParseInput);
+            yield return new Game(id, bags);
+        }
+    }
+
+    partial record class GameReveal(int Red, int Blue, int Green)
+    {
+        [GeneratedRegex("(?<amount>\\d+) (?<color>red|blue|green)")]
+        private static partial Regex ColorRegex();
+
+        public static GameReveal ParseInput(string input)
+        {
+            var red = 0;
+            var blue = 0;
+            var green = 0;
+
+            foreach (var match in input.Split(',').Select(s => ColorRegex().Match(s)))
+            {
+                if (!match.Success)
+                {
+                    throw new Exception("Unable to parse color");
+                }
+
+                var color = match.Groups["color"].Value;
+                var value = int.Parse(match.Groups["amount"].Value);
+                if (color == "red")
+                {
+                    red += value;
+                }
+                else if (color == "blue")
+                {
+                    blue += value;
+                }
+                else if (color == "green")
+                {
+                    green += value;
+                }
+            }
+
+            return new GameReveal(red, blue, green);
+        }
+    }
+
+    record Game(int Id, IEnumerable<GameReveal> Reveals);
+}
