ed25a1cef7f91c2fcd41ab3f06f2707ef1bdd6d5
Rory Claasen <rory@xbox.com>
2023-12-12 14:17:06 +0000

    Adds 2023 Day 9

---
+++ source/AdventOfCode.Year2023/Day9/Day9Solution.cs
@@ -0,0 +1,74 @@
+namespace AdventOfCode.Year2023;
+
+using AdventOfCode.Shared;
+using System.Collections.Generic;
+using System.Linq;
+
+[Problem(2023, 9, "Mirage Maintenance")]
+public class Day9Solution : ISolver
+{
+    public object? PartOne(string input)
+    {
+        var total = 0;
+        foreach (var numbers in this.ParseInput(input))
+        {
+            var diffs = this.GetDiffs(numbers);
+
+            for (var i = diffs.Count - 1; i > 0; i--)
+            {
+                diffs[i - 1].Add(diffs[i - 1][^1] + diffs[i][^1]);
+            }
+
+            total += diffs[0][^1];
+        }
+
+        return total;
+    }
+
+    public object? PartTwo(string input)
+    {
+        var total = 0;
+        foreach (var numbers in this.ParseInput(input))
+        {
+            var diffs = this.GetDiffs(numbers);
+
+            for (var i = diffs.Count - 1; i > 0; i--)
+            {
+                diffs[i - 1].Insert(0, diffs[i - 1][0] - diffs[i][0]);
+            }
+
+            total += diffs[0][0];
+        }
+
+        return total;
+    }
+
+    private List<List<int>> GetDiffs(List<int> input)
+    {
+        var diffs = new List<List<int>> { input };
+
+        var current = new List<int>(input);
+        while (current.Any(x => x != 0))
+        {
+            current = [];
+
+            for (var i = 0; i < diffs[^1].Count - 1; i++)
+            {
+                var diff = diffs[^1][i + 1] - diffs[^1][i];
+                current.Add(diff);
+            }
+
+            diffs.Add(current);
+        }
+
+        return diffs;
+    }
+
+    private IEnumerable<List<int>> ParseInput(string input)
+    {
+        foreach (var line in input.Lines())
+        {
+            yield return line.Split(' ').Select(int.Parse).ToList();
+        }
+    }
+}
---
+++ tests/AdventOfCode.Year2023.Tests/Day9SolutionTests.cs
@@ -0,0 +1,32 @@
+namespace AdventOfCode.Year2023.Tests;
+
+using AdventOfCode.Shared.Tests;
+
+[TestClass]
+public class Day9SolutionTests : SolverBaseTests<Day9Solution>
+{
+    [TestMethod]
+    [DataRow("0 3 6 9 12 15", "18")]
+    [DataRow("1 3 6 10 15 21", "28")]
+    [DataRow("10 13 16 21 30 45", "68")]
+    [DataRow(@"0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45", "114")]
+    public void SolvePart1(string input, string answer)
+    {
+        Assert.IsNotNull(this.Solver);
+
+        var solvedAnswer = this.Solver.PartOne(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+
+    [TestMethod]
+    [DataRow("10 13 16 21 30 45", "5")]
+    public void SolvePart2(string input, string answer)
+    {
+        Assert.IsNotNull(this.Solver);
+
+        var solvedAnswer = this.Solver.PartTwo(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+}
