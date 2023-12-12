4b2416d74ba192bb75ecd6ba519a31936ba65128
Rory Claasen <rory@xbox.com>
2023-12-12 13:43:31 +0000

    Adds 2023 Day 8

---
+++ source/AdventOfCode.Year2023/Day8/Day8Solution.cs
@@ -0,0 +1,82 @@
+namespace AdventOfCode.Year2023;
+
+using AdventOfCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+[Problem(2023, 8, "Haunted Wasteland")]
+public partial class Day8Solution : ISolver
+{
+    public object? PartOne(string input)
+    {
+        var (directions, mappings) = ParseInput(input);
+        var steps = 0;
+        var current = "AAA";
+        do
+        {
+            var direction = directions[steps % directions.Length];
+            current = this.GetNextKey(current, mappings, direction);
+            steps++;
+        } while (current != "ZZZ");
+        return steps;
+    }
+
+    public object? PartTwo(string input)
+    {
+        var (directions, mappings) = this.ParseInput(input);
+
+        var allPaths = new List<long>();
+        foreach (var node in mappings.Keys.Where(s => s.EndsWith('A')))
+        {
+            var steps = 0;
+            var current = node;
+            do
+            {
+                var direction = directions[steps % directions.Length];
+                current = this.GetNextKey(current, mappings, direction);
+                steps++;
+            } while (!current.EndsWith('Z'));
+            allPaths.Add(steps);
+        }
+        return MathUtils.Lcm(allPaths);
+    }
+
+    private string GetNextKey(string current, Dictionary<string, (string Left, string Right)> mappings, Direction direction) => direction switch
+    {
+        Direction.Left => mappings[current].Left,
+        Direction.Right => mappings[current].Right,
+        _ => throw new Exception("Invalid direction")
+    };
+
+    private (Direction[] Directions, Dictionary<string, (string Left, string Right)> Mapping) ParseInput(string input)
+    {
+        var parts = input.Lines(2);
+        var directions = parts[0].ToCharArray().Select(c => (Direction)c);
+        if (directions.Any(d => d == Direction.Up || d == Direction.Down))
+        {
+            throw new Exception("Invalid input (directions)");
+        }
+
+        var mappings = new Dictionary<string, (string, string)>();
+        foreach (var line in parts[1].Lines())
+        {
+            var match = NodeMapRegex().Match(line);
+            if (!match.Success)
+            {
+                throw new Exception($"Invalid input '{line}'");
+            }
+
+            var source = match.Groups[1].Value;
+            var left = match.Groups[2].Value;
+            var right = match.Groups[3].Value;
+            mappings.Add(source, (left, right));
+        }
+
+        return (directions.ToArray(), mappings);
+    }
+
+    [GeneratedRegex(@"(\w{3}) = \((\w{3}), (\w{3})\)")]
+    private static partial Regex NodeMapRegex();
+}
---
+++ tests/AdventOfCode.Year2023.Tests/Day8SolutionTests.cs
@@ -0,0 +1,49 @@
+namespace AdventOfCode.Year2023.Tests;
+
+using AdventOfCode.Shared.Tests;
+
+[TestClass]
+public class Day8SolutionTests : SolverBaseTests<Day8Solution>
+{
+    [TestMethod]
+    [DataRow(@"RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)", "2")]
+    [DataRow(@"LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)", "6")]
+    public void SolvePart1(string input, string answer)
+    {
+        Assert.IsNotNull(this.Solver);
+
+        var solvedAnswer = this.Solver.PartOne(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+
+    [TestMethod]
+    [DataRow(@"LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)", "6")]
+    public void SolvePart2(string input, string answer)
+    {
+        Assert.IsNotNull(this.Solver);
+
+        var solvedAnswer = this.Solver.PartTwo(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+}
