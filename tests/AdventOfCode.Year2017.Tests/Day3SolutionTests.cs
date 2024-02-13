eaf4cf68f3e0cbbd1554fea2e8f4758283ec553b
Rory Claasen <rory@xbox.com>
2024-02-13 11:03:03 +0000

    Adds half of 2017 Day 3

---
+++ tests/AdventOfCode.Year2017.Tests/Day3SolutionTests.cs
@@ -0,0 +1,30 @@
+namespace AdventOfCode.Year2017.Tests;
+
+using AdventOfCode.Shared.Tests;
+
+[TestClass]
+public class Day3SolutionTests : SolverBaseTests<Day3Solution>
+{
+    [TestMethod]
+    [DataRow("1", "0")]
+    [DataRow("12", "3")]
+    [DataRow("23", "2")]
+    [DataRow("1024", "31")]
+    public void SolvePart1(string input, string answer)
+    {
+        Assert.IsNotNull(this.Solver);
+
+        var solvedAnswer = this.Solver.PartOne(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+
+    [TestMethod]
+    [DataRow("", "")]
+    public void SolvePart2(string input, string answer)
+    {
+        Assert.IsNotNull(this.Solver);
+
+        var solvedAnswer = this.Solver.PartTwo(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+}
