fcd2f634cd8e10062b2c77c8c8a4125501c38fe2
Rory Claasen <rory@xbox.com>
2024-03-10 14:37:36 -0700

    Adds 2918 day 1

---
+++ tests/AdventOfCode.Year2018.Tests/Day1SolutionTests.cs
@@ -0,0 +1,35 @@
+namespace AdventOfCode.Year2018.Tests;
+
+using AdventOfCode.Shared.Tests;
+using System;
+
+[TestClass]
+public class Day1SolutionTests : SolverBaseTests<Day1Solution>
+{
+    [TestMethod]
+    [DataRow("+1, -2, +3, +1", "3")]
+    [DataRow("+1, +1, +1", "3")]
+    [DataRow("+1, +1, -2", "0")]
+    [DataRow("-1, -2, -3", "-6")]
+    public void SolvePart1(string input, string answer)
+    {
+        Assert.IsNotNull(this.Solver);
+
+        var solvedAnswer = this.Solver.PartOne(input.Replace(", ", Environment.NewLine))?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+
+    [TestMethod]
+    [DataRow("+1, -2, +3, +1", "2")]
+    [DataRow("+1, -1", "0")]
+    [DataRow("+3, +3, +4, -2, -4", "10")]
+    [DataRow("-6, +3, +8, +5, -6", "5")]
+    [DataRow("+7, +7, -2, -7, -4", "14")]
+    public void SolvePart2(string input, string answer)
+    {
+        Assert.IsNotNull(this.Solver);
+
+        var solvedAnswer = this.Solver.PartTwo(input.Replace(", ", Environment.NewLine))?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+}
