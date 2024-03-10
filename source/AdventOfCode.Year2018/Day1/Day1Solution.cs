4111202cc06ed29b89a6f3e416e9a587d8dfeaad
Rory Claasen <rory@xbox.com>
2024-03-10 14:37:36 -0700

    Adds 2918 day 1

---
+++ source/AdventOfCode.Year2018/Day1/Day1Solution.cs
@@ -0,0 +1,35 @@
+namespace AdventOfCode.Year2018;
+
+using AdventOfCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+[Problem(2018, 1, "Chronal Calibration")]
+public class Day1Solution : ISolver
+{
+    public object? PartOne(string input)
+        => ParseInput(input).Sum();
+
+    public object? PartTwo(string input)
+    {
+        const int maxIterations = 10000;
+        var changes = ParseInput(input).ToArray();
+        var history = new HashSet<int>() { 0 };
+        var frequency = 0;
+        for (var i = 0; i < changes.Length * maxIterations; i++)
+        {
+            var change = changes[i % changes.Length];
+            frequency += change;
+            if (!history.Add(frequency))
+            {
+                return frequency;
+            }
+        }
+
+        throw new Exception($"Unable to find a solution after {maxIterations} iterations");
+    }
+
+    private static IEnumerable<int> ParseInput(string input)
+        => input.Lines().Select(int.Parse);
+}
