339bc0211a1a6e8b21d2c4d8db2a87ec554de008
Rory Claasen <rory@xbox.com>
2024-03-10 17:11:57 -0700

    adds 2018 day 2

---
+++ tests/AdventOfCode.Year2018.Tests/Day2SolutionTests.cs
@@ -0,0 +1,39 @@
+namespace AdventOfCode.Year2018.Tests;
+
+using AdventOfCode.Shared.Tests;
+
+[TestClass]
+public class Day2SolutionTests : SolverBaseTests<Day2Solution>
+{
+    [TestMethod]
+    [DataRow(@"abcdef
+bababc
+abbcde
+abcccd
+aabcdd
+abcdee
+ababab", "12")]
+    public void SolvePart1(string input, string answer)
+    {
+        Assert.IsNotNull(this.Solver);
+
+        var solvedAnswer = this.Solver.PartOne(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+
+    [TestMethod]
+    [DataRow(@"abcde
+fghij
+klmno
+pqrst
+fguij
+axcye
+wvxyz", "fgij")]
+    public void SolvePart2(string input, string answer)
+    {
+        Assert.IsNotNull(this.Solver);
+
+        var solvedAnswer = this.Solver.PartTwo(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+}
