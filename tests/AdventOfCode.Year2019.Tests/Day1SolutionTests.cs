f693ae536270f079522d03113e0cab2d9e46ac0a
Rory Claasen <rory@claasen.biz>
2024-03-23 22:23:24 +0000

    Adds 2019 day 1

---
+++ tests/AdventOfCode.Year2019.Tests/Day1SolutionTests.cs
@@ -0,0 +1,32 @@
+namespace AdventOfCode.Year2019.Tests;
+
+using AdventOfCode.Shared.Tests;
+
+[TestClass]
+public class Day1SolutionTests : SolverBaseTests<Day1Solution>
+{
+    [TestMethod]
+    [DataRow("12", "2")]
+    [DataRow("14", "2")]
+    [DataRow("1969", "654")]
+    [DataRow("100756", "33583")]
+    public void SolvePart1(string input, string answer)
+    {
+        Assert.IsNotNull(this.Solver);
+
+        var solvedAnswer = this.Solver.PartOne(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+
+    [TestMethod]
+    [DataRow("14", "2")]
+    [DataRow("1969", "966")]
+    [DataRow("100756", "50346")]
+    public void SolvePart2(string input, string answer)
+    {
+        Assert.IsNotNull(this.Solver);
+
+        var solvedAnswer = this.Solver.PartTwo(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+}
