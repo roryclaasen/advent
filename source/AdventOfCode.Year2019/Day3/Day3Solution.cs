01430c748607aa51b4655431efd1b3af0613af3a
Rory Claasen <rory@claasen.biz>
2024-04-05 22:18:18 +0100

    Add Day3Solution and Day3SolutionTests

---
+++ source/AdventOfCode.Year2019/Day3/Day3Solution.cs
@@ -0,0 +1,75 @@
+namespace AdventOfCode.Year2019;
+
+using AdventOfCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+[Problem(2019, 3, "Crossed Wires")]
+public class Day3Solution : ISolver
+{
+    public object? PartOne(string input)
+    {
+        var wires = ParseInput(input);
+
+        var wire1Path = GetPath(wires[0]);
+        var wire2Path = GetPath(wires[1]);
+        return wire1Path
+            .Intersect(wire2Path, new Vector3ComparerAsVector2())
+            .Select(s => Math.Abs(s.X) + Math.Abs(s.Y))
+            .Min();
+    }
+
+    public object? PartTwo(string input)
+    {
+        var wires = ParseInput(input);
+
+        var wire1Path = GetPath(wires[0]);
+        var wire2Path = GetPath(wires[1]);
+        return wire1Path
+            .Intersect(wire2Path, new Vector3ComparerAsVector2())
+            .Select(i => wire1Path.First(s => s.X == i.X && s.Y == i.Y).Z + wire2Path.First(s => s.X == i.X && s.Y == i.Y).Z)
+            .Min();
+    }
+
+    private IEnumerable<Vector3> GetPath(IEnumerable<Instruction> instructions)
+    {
+        var current = Vector3.Zero;
+
+        foreach (var instruction in instructions)
+        {
+            for (var i = 0; i < instruction.Distance; i++)
+            {
+                current = instruction.Direction switch
+                {
+                    Direction.Up => new Vector3(current.X, current.Y + 1, current.Z + 1),
+                    Direction.Right => new Vector3(current.X + 1, current.Y, current.Z + 1),
+                    Direction.Down => new Vector3(current.X, current.Y - 1, current.Z + 1),
+                    Direction.Left => new Vector3(current.X - 1, current.Y, current.Z + 1),
+                    _ => throw new InvalidOperationException()
+                };
+
+                yield return current;
+            }
+        }
+    }
+
+    private IEnumerable<Instruction>[] ParseInput(string input)
+    {
+        static IEnumerable<Instruction> ParseLine(string line) => line
+            .Split(',')
+            .Select(ins => new Instruction((Direction)ins[0], int.Parse(ins[1..])));
+
+        return input.Lines().Select(ParseLine).ToArray();
+    }
+
+    record Instruction(Direction Direction, int Distance);
+
+    private class Vector3ComparerAsVector2 : IEqualityComparer<Vector3>
+    {
+        public bool Equals(Vector3 a, Vector3 b) => a.X == b.X && a.Y == b.Y;
+
+        public int GetHashCode(Vector3 obj) => HashCode.Combine(obj.X, obj.Y);
+    }
+}
---
+++ tests/AdventOfCode.Year2019.Tests/Day3SolutionTests.cs
@@ -0,0 +1,33 @@
+namespace AdventOfCode.Year2019.Tests;
+
+using AdventOfCode.Shared.Tests;
+
+[TestClass]
+public class Day3SolutionTests : SolverBaseTests<Day3Solution>
+{
+    [TestMethod]
+    [DataRow(@"R8,U5,L5,D3
+U7,R6,D4,L4", "6")]
+    [DataRow(@"R75,D30,R83,U83,L12,D49,R71,U7,L72
+U62,R66,U55,R34,D71,R55,D58,R83", "159")]
+    [DataRow(@"R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+U98,R91,D20,R16,D67,R40,U7,R15,U6,R7", "135")]
+    public void SolvePart1(string input, string answer)
+    {
+        var solvedAnswer = this.Solver.PartOne(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+
+    [TestMethod]
+    [DataRow(@"R8,U5,L5,D3
+U7,R6,D4,L4", "30")]
+    [DataRow(@"R75,D30,R83,U83,L12,D49,R71,U7,L72
+U62,R66,U55,R34,D71,R55,D58,R83", "610")]
+    [DataRow(@"R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+U98,R91,D20,R16,D67,R40,U7,R15,U6,R7", "410")]
+    public void SolvePart2(string input, string answer)
+    {
+        var solvedAnswer = this.Solver.PartTwo(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+}
