6ed54b5cda6cb630b70aa94e78d738edd4317ff7
Rory Claasen <rory@xbox.com>
2024-12-03 09:41:50 +0000

    Refactor Day3Solution to remove input parameters from PartOne and PartTwo methods

--- source/AdventOfCode.Year2024/Day3/Day3Solution.cs
+++ source/AdventOfCode.Year2024/Day3/Day3Solution.cs
@@ -7,14 +7,14 @@ using System.Linq;
 [Problem(2024, 3, "Mull It Over")]
 public partial class Day3Solution : ISolver
 {
-    public object? PartOne(string input)
+    public object? PartOne()
         => SequenceRegex()
             .Matches(input)
             .Where(m => m.Groups[0].Value.StartsWith("mul"))
             .Select(m => (int.Parse(m.Groups[1].ValueSpan), int.Parse(m.Groups[2].ValueSpan)))
             .Sum(m => m.Item1 * m.Item2);
 
-    public object? PartTwo(string input)
+    public object? PartTwo()
     {
         var enabled = true;
         var total = 0;
