6c5d59cc0768c70506a9ceb40e14e4c925824af8
Rory Claasen <rory@xbox.com>
2024-12-03 09:40:48 +0000

    Adds 2024 Day 3

---
+++ tests/AdventOfCode.Year2024.Tests/Day3SolutionTests.cs
@@ -0,0 +1,23 @@
+namespace AdventOfCode.Year2024.Tests;
+
+using AdventOfCode.Shared.Tests;
+
+[TestClass]
+public class Day3SolutionTests : SolverBaseTests<Day3Solution>
+{
+    [TestMethod]
+    [DataRow("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))", "161")]
+    public void SolvePart1(string input, string answer)
+    {
+        var solvedAnswer = this.Solver.PartOne(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+
+    [TestMethod]
+    [DataRow("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))", "48")]
+    public void SolvePart2(string input, string answer)
+    {
+        var solvedAnswer = this.Solver.PartTwo(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+}
