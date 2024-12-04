8558352d7aa07424d0a17588706cac65ced71c1c
Rory Claasen <rory@xbox.com>
2024-12-04 14:54:27 +0000

    Adds 2024 Day 4

---
+++ tests/AdventOfCode.Year2024.Tests/Day4SolutionTests.cs
@@ -0,0 +1,44 @@
+namespace AdventOfCode.Year2024.Tests;
+
+using AdventOfCode.Shared.Tests;
+
+[TestClass]
+public class Day4SolutionTests : SolverBaseTests<Day4Solution>
+{
+    [TestMethod]
+    [DataRow(@"MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX", "18")]
+    public void SolvePart1(string input, string answer)
+    {
+        var solvedAnswer = this.Solver.PartOne(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+
+    [TestMethod]
+    [DataRow(@"M.S
+.A.
+M.S", "1")]
+    [DataRow(@"MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX", "9")]
+    public void SolvePart2(string input, string answer)
+    {
+        var solvedAnswer = this.Solver.PartTwo(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+}
