d4a3d15977cf65165d24a493d6f3e84179831305
Rory Claasen <rory@claasen.biz>
2024-12-07 09:26:10 +0000

    Adds 2024 day 7

--- source/AdventOfCode.Shared/MathUtils.cs
+++ source/AdventOfCode.Shared/MathUtils.cs
@@ -41,4 +41,30 @@ public static class MathUtils
     /// <returns>The least common multiple.</returns>
     public static TNumber Lcm<TNumber>(IEnumerable<TNumber> numbers) where TNumber : struct, INumber<TNumber>
         => numbers.Aggregate(Lcm);
+
+    /// <summary>
+    /// Fast concatenation of two uints.
+    /// <see href="https://stackoverflow.com/a/26853517/4498839"/>
+    /// </summary>
+    /// <param name="a">The first number.</param>
+    /// <param name="b">The second number.</param>
+    /// <returns>The concatenated number.</returns>
+    public static ulong FastConcat(ulong a, ulong b)
+    {
+        checked
+        {
+            return b switch
+            {
+                (< 10U) => 10UL * a + b,
+                (< 100U) => 100UL * a + b,
+                (< 1000U) => 1000UL * a + b,
+                (< 10000U) => 10000UL * a + b,
+                (< 100000U) => 100000UL * a + b,
+                (< 1000000U) => 1000000UL * a + b,
+                (< 10000000U) => 10000000UL * a + b,
+                (< 100000000U) => 100000000UL * a + b,
+                _ => 1000000000UL * a + b
+            };
+        }
+    }
 }
---
+++ tests/AdventOfCode.Year2024.Tests/Day7SolutionTests.cs
@@ -0,0 +1,39 @@
+namespace AdventOfCode.Year2024.Tests;
+
+using AdventOfCode.Shared.Tests;
+
+[TestClass]
+public class Day7SolutionTests : SolverBaseTests<Day7Solution>
+{
+    [TestMethod]
+    [DataRow(@"190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20", "3749")]
+    public void SolvePart1(string input, string answer)
+    {
+        var solvedAnswer = this.Solver.PartOne(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+
+    [TestMethod]
+    [DataRow(@"190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20", "11387")]
+    public void SolvePart2(string input, string answer)
+    {
+        var solvedAnswer = this.Solver.PartTwo(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+}
