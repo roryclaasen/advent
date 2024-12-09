15f5773b7926973ba9af4985515d088ff8835d38
Rory Claasen <rory@xbox.com>
2024-12-09 14:20:12 +0000

    Enhance MathUtils: Refactor FastConcat method and improve documentation

--- source/AdventOfCode.Shared/MathUtils.cs
+++ source/AdventOfCode.Shared/MathUtils.cs
@@ -1,5 +1,6 @@
 namespace AdventOfCode.Shared;
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
@@ -43,9 +44,11 @@ public static class MathUtils
         => numbers.Aggregate(Lcm);
 
     /// <summary>
-    /// Fast concatenation of two uints.
-    /// <see href="https://stackoverflow.com/a/26853517/4498839"/>
+    /// Fast concatenation of two numbers.
     /// </summary>
+    /// <remarks>
+    /// Adapted from <see href="https://stackoverflow.com/a/26853517/4498839"/>
+    /// </remarks>
     /// <param name="a">The first number.</param>
     /// <param name="b">The second number.</param>
     /// <returns>The concatenated number.</returns>
@@ -55,16 +58,30 @@ public static class MathUtils
         {
             return b switch
             {
-                (< 10U) => 10UL * a + b,
-                (< 100U) => 100UL * a + b,
-                (< 1000U) => 1000UL * a + b,
-                (< 10000U) => 10000UL * a + b,
-                (< 100000U) => 100000UL * a + b,
-                (< 1000000U) => 1000000UL * a + b,
-                (< 10000000U) => 10000000UL * a + b,
-                (< 100000000U) => 100000000UL * a + b,
-                _ => 1000000000UL * a + b
+                < 10UL => 10UL * a + b,
+                < 100UL => 100UL * a + b,
+                < 1000UL => 1000UL * a + b,
+                < 10000UL => 10000UL * a + b,
+                < 100000UL => 100000UL * a + b,
+                < 1000000UL => 1000000UL * a + b,
+                < 10000000UL => 10000000UL * a + b,
+                < 100000000UL => 100000000UL * a + b,
+                _ => 1000000000UL * a + b,
             };
+        }
+    }
+
+    /// <summary>
+    /// Fast concatenation of two numbers.
+    /// </summary>
+    /// <param name="a">The first number.</param>
+    /// <param name="b">The second number.</param>
+    /// <returns>The concatenated number.</returns>
+    public static double FastConcat(double a, double b)
+    {
+        checked
+        {
+            return a * Math.Pow(10, Math.Floor(Math.Log10(b)) + 1) + b;
         }
     }
 }
