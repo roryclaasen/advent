e609cac6ab0f7c23f2c3a50aa8dcc875413fb07b
Rory Claasen <rory@claasen.biz>
2025-10-03 20:43:08 +0100

    Add Day 1 solution and tests for Advent of Code 2025
    Introduced `Day1Solution` class in `Day1Solution.cs` under the
    `AdventOfCode.Year2025` namespace. This class implements the
    `IProblemSolver` interface and includes placeholder methods
    for solving Part 1 and Part 2 of the problem.
    
    Added `Day1SolutionTests` class in `Day1SolutionTests.cs` under
    the `AdventOfCode.Year2025.Tests` namespace. This class
    inherits from `SolverBaseTests<Day1Solution>` and includes
    parameterized test methods for verifying solutions to Part 1
    and Part 2 using the `DataRow` attribute.

---
+++ source/AdventOfCode.Year2025/Day1/Day1Solution.cs
@@ -0,0 +1,17 @@
+namespace AdventOfCode.Year2025;
+
+using AdventOfCode.Problem;
+
+[Problem(2025, 1)]
+public partial class Day1Solution : IProblemSolver
+{
+    public object? PartOne(string input)
+    {
+        return string.Empty;
+    }
+
+    public object? PartTwo(string input)
+    {
+        return string.Empty;
+    }
+}
---
+++ tests/AdventOfCode.Year2025.Tests/Day1SolutionTests.cs
@@ -0,0 +1,23 @@
+namespace AdventOfCode.Year2025.Tests;
+
+using AdventOfCode.Shared.Tests;
+
+[TestClass]
+public class Day1SolutionTests : SolverBaseTests<Day1Solution>
+{
+    [TestMethod]
+    [DataRow("", "")]
+    public void SolvePart1(string input, string answer)
+    {
+        var solvedAnswer = this.Solver.PartOne(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+
+    [TestMethod]
+    [DataRow("", "")]
+    public void SolvePart2(string input, string answer)
+    {
+        var solvedAnswer = this.Solver.PartTwo(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+}
