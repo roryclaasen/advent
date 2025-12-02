cfe2fdd804d5d87482f479f7fb6303f9f42ce393
Rory Claasen <rory@xbox.com>
2025-12-02 14:48:21 +0000

    Add Day 1 solution logic and update tests and inputs
    Implemented `PartOne` and `PartTwo` methods in `Day1Solution` to calculate position clicks based on directional instructions. Added a `ParseInput` method and `Instruction` record struct to handle input parsing. Updated `Expected1.txt`, `Expected2.txt`, and `Input.txt` with test data. Enhanced `Day1SolutionTests` with new test cases for both parts.

--- source/AdventOfCode.Year2025/Day1/Day1Solution.cs
+++ source/AdventOfCode.Year2025/Day1/Day1Solution.cs
@@ -3,18 +3,89 @@
 
 namespace AdventOfCode.Year2025;
 
+using System;
+using System.Collections.Generic;
 using AdventOfCode.Problem;
+using AdventOfCode.Shared;
 
-[Problem(2025, 1)]
+[Problem(2025, 1, "Secret Entrance")]
 public partial class Day1Solution : IProblemSolver
 {
     public object? PartOne(string input)
     {
-        return string.Empty;
+        var position = 50;
+        var clicks = 0;
+        foreach (var ins in ParseInput(input))
+        {
+            position = ins.Direction switch
+            {
+                'L' => position - ins.Offset,
+                'R' => position + ins.Offset,
+                _ => throw new InvalidOperationException("Invalid direction")
+            };
+
+            while (position < 0 || position > 99)
+            {
+                if (position > 99)
+                {
+                    position -= 100;
+                }
+                else if (position < 0)
+                {
+                    position += 100;
+                }
+            }
+
+            if (position == 0)
+            {
+                clicks++;
+            }
+        }
+
+        return clicks;
     }
 
     public object? PartTwo(string input)
     {
-        return string.Empty;
+        var position = 50;
+        var clicks = 0;
+        foreach (var (direction, offset) in ParseInput(input))
+        {
+            for (var i = 1; i <= offset; i++)
+            {
+                position = direction switch
+                {
+                    'L' => position - 1,
+                    'R' => position + 1,
+                    _ => throw new InvalidOperationException("Invalid direction")
+                };
+
+                if (position > 99)
+                {
+                    position = 0;
+                }
+                else if (position < 0)
+                {
+                    position = 99;
+                }
+
+                if (position == 0)
+                {
+                    clicks++;
+                }
+            }
+        }
+
+        return clicks;
     }
+
+    private static IEnumerable<Instruction> ParseInput(string input)
+    {
+        foreach (var line in input.Lines())
+        {
+            yield return new Instruction(line[0], int.Parse(line[1..]));
+        }
+    }
+
+    private record struct Instruction(char Direction, int Offset);
 }
--- tests/AdventOfCode.Year2025.Tests/Day1SolutionTests.cs
+++ tests/AdventOfCode.Year2025.Tests/Day1SolutionTests.cs
@@ -9,7 +9,16 @@ using AdventOfCode.Shared.Tests;
 public class Day1SolutionTests : SolverBaseTests<Day1Solution>
 {
     [TestMethod]
-    [DataRow("", "")]
+    [DataRow(@"L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82", "3")]
     public void SolvePart1(string input, string answer)
     {
         var solvedAnswer = this.Solver.PartOne(input)?.ToString();
@@ -17,7 +26,17 @@ public class Day1SolutionTests : SolverBaseTests<Day1Solution>
     }
 
     [TestMethod]
-    [DataRow("", "")]
+    [DataRow(@"L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82", "6")]
+    [DataRow("R1000", "10")]
     public void SolvePart2(string input, string answer)
     {
         var solvedAnswer = this.Solver.PartTwo(input)?.ToString();
