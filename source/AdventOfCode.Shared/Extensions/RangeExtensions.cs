a6814fb8a2a9086f20c60cbe676f13df7c64c7d7
Rory Claasen <rory@xbox.com>
2025-12-02 16:43:46 +0000

    Refactor and extend range utilities in RangeExtensions
    Refactored the `RangeExtensions` class to improve clarity and functionality.
    Reorganized the `ToRange` method and removed the `ToEnumerable` method,
    replacing it with an inline implementation. Added a new `GetEnumerator`
    method and overloads to support tuple-based ranges and generic numeric
    types using `INumber<TNumber>`.
    
    Introduced the `RangeEnumerator<TNumber>` struct to enable efficient
    and type-safe iteration over numeric ranges. This struct implements
    `IEnumerator<TNumber>` and supports value-based iteration for ranges
    defined with numeric types. The changes improve usability, expand
    support for generic numeric ranges, and enhance code organization.

--- source/AdventOfCode.Shared/Extensions/RangeExtensions.cs
+++ source/AdventOfCode.Shared/Extensions/RangeExtensions.cs
@@ -6,15 +6,24 @@ namespace AdventOfCode.Shared.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
+using AdventOfCode.Shared.Numerics;
 
 public static class RangeExtensions
 {
-    /// <summary>
-    /// Converts a collection of two integers to a <see cref="Range"/>.
-    /// </summary>
-    /// <param name="values">The enumerable.</param>
-    /// <returns>The range.</returns>
-    /// <exception cref="ArgumentException">Throws when <paramref name="values"/> doesn't contain exactly 2 values.</exception>
+    extension(Range range)
+    {
+        public IEnumerable<int> ToEnumerable()
+        {
+            for (int i = range.Start.Value; i <= range.End.Value; i++)
+            {
+                yield return i;
+            }
+        }
+
+        public RangeEnumerator<int> GetEnumerator() => new(range.Start.Value, range.End.Value);
+    }
+
     public static Range ToRange(this IEnumerable<int> values)
     {
         if (values.Count() != 2)
@@ -25,16 +34,11 @@ public static class RangeExtensions
         return new Range(values.First(), values.Last());
     }
 
-    /// <summary>
-    /// Converts a range to an enumerable.
-    /// </summary>
-    /// <param name="range">The range.</param>
-    /// <returns>An enumerable of all the values from the start to the end of the <paramref name="range"/></returns>
-    public static IEnumerable<int> ToEnumerable(this Range range)
-    {
-        for (int i = range.Start.Value; i <= range.End.Value; i++)
-        {
-            yield return i;
-        }
-    }
+    public static RangeEnumerator<TNumber> GetEnumerator<TNumber>(this (TNumber Start, TNumber End) range)
+        where TNumber : struct, INumber<TNumber>
+        => new(range.Start, range.End);
+
+    public static RangeEnumerator<TNumber> GetEnumerator<TNumber>(this Tuple<TNumber, TNumber> range)
+        where TNumber : struct, INumber<TNumber>
+        => new(range.Item1, range.Item2);
 }
---
+++ source/AdventOfCode.Shared/Numerics/RangeEnumerator.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Rory Claasen. All rights reserved.
+// Licensed under the MIT license. See LICENSE in the project root for license information.
+
+namespace AdventOfCode.Shared.Numerics;
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+
+public ref struct RangeEnumerator<TNumber> : IEnumerator<TNumber>
+    where TNumber : struct, INumber<TNumber>
+{
+    private readonly TNumber start;
+    private readonly TNumber end;
+
+    private TNumber current;
+
+    public RangeEnumerator(TNumber start, TNumber end)
+    {
+        this.start = start;
+        this.end = end;
+
+        this.current = this.start - TNumber.One;
+    }
+
+    public readonly TNumber Current => this.current;
+
+    readonly object IEnumerator.Current => this.current;
+
+    public bool MoveNext()
+    {
+        if (this.current < this.end)
+        {
+            this.current++;
+            return true;
+        }
+
+        return false;
+    }
+
+    public void Reset() => this.current = this.start;
+
+    public readonly void Dispose()
+    {
+    }
+}
