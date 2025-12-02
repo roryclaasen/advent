b2b8f70af0f8f9172145556ad72e4483909a5948
Rory Claasen <rory@xbox.com>
2025-12-02 16:44:12 +0000

    Add Day 2 solution and tests for Advent of Code 2025
    Added `Day2Solution` to solve the "Gift Shop" problem for Day 2, implementing both Part One and Part Two solutions. Introduced helper methods for input parsing and number pattern analysis. Updated `Expected1.txt` and `Expected2.txt` with expected outputs and added `Input.txt` for problem input.
    
    Created `Day2SolutionTests` to validate the solution, including test cases for both parts. Added copyright and MIT license headers to all relevant files.

---
+++ tests/AdventOfCode.Year2025.Tests/Day2SolutionTests.cs
@@ -0,0 +1,26 @@
+// Copyright (c) Rory Claasen. All rights reserved.
+// Licensed under the MIT license. See LICENSE in the project root for license information.
+
+namespace AdventOfCode.Year2025.Tests;
+
+using AdventOfCode.Shared.Tests;
+
+[TestClass]
+public class Day2SolutionTests : SolverBaseTests<Day2Solution>
+{
+    [TestMethod]
+    [DataRow("11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124", "1227775554")]
+    public void SolvePart1(string input, string answer)
+    {
+        var solvedAnswer = this.Solver.PartOne(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+
+    [TestMethod]
+    [DataRow("11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124", "4174379265")]
+    public void SolvePart2(string input, string answer)
+    {
+        var solvedAnswer = this.Solver.PartTwo(input)?.ToString();
+        Assert.AreEqual(answer, solvedAnswer);
+    }
+}
